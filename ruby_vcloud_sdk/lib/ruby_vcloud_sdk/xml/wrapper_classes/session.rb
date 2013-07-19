41c794a15d30167c716e8034e15c6d3643404371
Harshawardhan Gadgil <hgadgil@vmware.com>
2013-07-19 16:39:40 -0700

    Remove dependency on admin link

--- ruby_vcloud_sdk/lib/ruby_vcloud_sdk/xml/wrapper_classes/session.rb
+++ ruby_vcloud_sdk/lib/ruby_vcloud_sdk/xml/wrapper_classes/session.rb
@@ -7,6 +7,13 @@ module VCloudSdk
           VCloudSdk::Xml::ADMIN_MEDIA_TYPE[:VCLOUD]}).first
       end
 
+      def org_link(name)
+        get_nodes("Link",
+                  {"type" => VCloudSdk::Xml::MEDIA_TYPE[:ORGANIZATION],
+                   "name" => name
+                  }).first
+      end
+
       def entity_resolver
         get_nodes("Link", {"type" =>
           VCloudSdk::Xml::MEDIA_TYPE[:ENTITY]}).first

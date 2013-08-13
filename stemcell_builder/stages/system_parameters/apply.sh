742c2125ff8a0ceb7a11ec7a0e12ca4fe78414ad
Yisui Hu <yisuih@vmware.com>
2013-08-13 16:56:32 -0700

    Revert "fix system parameters for bosh agent"
    This reverts commit ce9ce4e389121a2b124074acc8e153897a02a798.

--- stemcell_builder/stages/system_parameters/apply.sh
+++ stemcell_builder/stages/system_parameters/apply.sh
@@ -7,8 +7,4 @@ set -e
 base_dir=$(readlink -nf $(dirname $0)/../..)
 source $base_dir/lib/prelude_apply.bash
 
-# this parameters is used by bosh agent
-# we should use vsphere when building for vcloud
-infrastructure=$system_parameters_infrastructure
-[ "$infrastructure" == "vcloud" ] && infrastructure=vsphere
-echo -n $infrastructure > $chroot/etc/infrastructure
+echo -n $system_parameters_infrastructure > $chroot/etc/infrastructure
